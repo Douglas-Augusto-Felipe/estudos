7366297e304b90cba1a8e0a7f20548e31f3937d2
Douglas Augusto <dodo.dafs@gmail.com>
2023-06-21 17:41:21 -0300

    21/06/2023

---
+++ python/arquivo.py
@@ -0,0 +1,8 @@
+nome= input('qual seu nome ')
+idade= int(input('qual sua idade'))
+if (idade == 30):
+    print('não passou')
+elif (idade < 30) :    
+    print("passou")
+elif (idade > 30):
+    print("parabens")
